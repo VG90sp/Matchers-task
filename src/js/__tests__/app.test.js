06d374ab3999b494c12a554be7a5328c069fbab5
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2021-02-28 19:18:08 +0300

    fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -16,3 +16,18 @@ test('sort', () => {
 
   expect(sortByHealth(input)).toEqual(expected);
 });
+test('sort2', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(sortByHealth(input)).not.toBe(expected);
+});
